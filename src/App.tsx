dea35db534532dbc0523c4cf0394f71a297933f4
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 23:15:09 +0100

    Tailwind Tables

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,33 @@ function App() {
     <div className="page">
       <h1 className="title">Shop</h1>
 
+      <table className="table-auto w-full hover">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+          </tr>
+          <tr>
+            <td>Witchy Woman</td>
+            <td>The Eagles</td>
+            <td>1972</td>
+          </tr>
+          <tr>
+            <td>Shining Star</td>
+            <td>Earth, Wind, and Fire</td>
+            <td>1975</td>
+          </tr>
+        </tbody>
+      </table>
+
       <br />
       <input type="text" />
       <input type="text" className="error" />
