9ab601d004c7812eef0d46315b43b769f84d1393
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 18:50:58 +0100

    Tailwind Page responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 function App() {
   return (
-    <div>
-      <div className="title">Shop</div>
+    <div className="page">
+      <h1 className="title">Shop</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae veritatis
+      sint mollitia illo dicta eum in praesentium suscipit rem animi? Libero
+      esse nihil perferendis tenetur id totam dolore a quo.
     </div>
   );
 }
