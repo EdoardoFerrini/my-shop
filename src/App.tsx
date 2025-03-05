77a8173e75f188e69eb47b6c81f92aaba5807ae4
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 18:52:38 +0100

    Tailwind Creare Button e varianti

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ function App() {
   return (
     <div className="page">
       <h1 className="title">Shop</h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae veritatis
-      sint mollitia illo dicta eum in praesentium suscipit rem animi? Libero
-      esse nihil perferendis tenetur id totam dolore a quo.
+
+      <button className="btn">default</button>
+      <button className="btn dark">dark</button>
+      <button className="btn danger">danger</button>
+      <button className="btn primary">primary</button>
+      <button className="btn accent">accent</button>
+      <button className="btn success">success</button>
+      <button className="btn success outline">outline</button>
+      <button className="btn outline">outline</button>
+      <button className="btn primary">base</button>
+      <button className="btn primary lg">lg</button>
     </div>
   );
 }
