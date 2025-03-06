28340a3b8deb75e69e13cca00c983f250628b024
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-06 13:02:51 +0100

    Installare React Router e creare le regole

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ShopPage } from "./pages/shop/ShopPage";
+import { CartPage } from "./pages/cart/CartPage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { LoginPage } from "./pages/login/LoginPage";
+import { ThanksPage } from "./pages/checkout/ThanksPage";
+import { CMSPage } from "./pages/cms/CMSPage";
+import { CMSProductsPage } from "./pages/cms/products/CMSProductsPage";
+import { CMSOrdersPage } from "./pages/cms/orders/CMSOrdersPage";
 
 function App() {
   return (
-    <div className="page">
-      <ShopPage />
-    </div>
+    <BrowserRouter>
+      <div className="page">
+        <Routes>
+          <Route path="shop" element={<ShopPage />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="checkout" element={<CheckoutPage />} />
+          <Route path="thankyou" element={<ThanksPage />} />
+          <Route path="login" element={<LoginPage />} />
+
+          <Route path="cms" element={<CMSPage />}>
+            <Route path="products" element={<CMSProductsPage />}></Route>
+            <Route path="orders" element={<CMSOrdersPage />}></Route>
+            <Route index element={<Navigate to="products" />}></Route>
+          </Route>
+
+          <Route path="*" element={<Navigate to="shop" />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
