d043d0fe7cf894fc1c2c179b8d20ea8dc0dafd0e
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-07 16:58:04 +0100

    L'hook useEffect

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,23 +1,29 @@
 import { Product } from "@/model/product";
 import PocketBase from "pocketbase";
+import { useEffect, useState } from "react";
 export const pb = new PocketBase("http://127.0.0.1:8090");
 
 export function ShopPage() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   function loadData() {
     pb.collection("products")
       .getList<Product>()
       .then((res) => {
-        console.log(res.items[0].name);
+        setProducts(res.items);
       });
   }
 
+  console.log(products);
   return (
     <div>
       <h1 className="title">Shop</h1>
 
-      <button className="btn" onClick={loadData}>
-        load data
-      </button>
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </div>
   );
 }
