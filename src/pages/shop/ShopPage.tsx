c80a5f66237ea61e594d07cd37166c8c2de02ba8
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-07 13:43:37 +0100

    Installare e usare l'SDK JavaScript di PocketBase

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,8 +1,22 @@
+import PocketBase from "pocketbase";
+export const pb = new PocketBase("http://127.0.0.1:8090");
+
 export function ShopPage() {
+  function loadData() {
+    pb.collection("products")
+      .getList()
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
   return (
     <div>
       <h1 className="title">Shop</h1>
-      content here
+
+      <button className="btn" onClick={loadData}>
+        load data
+      </button>
     </div>
   );
 }
