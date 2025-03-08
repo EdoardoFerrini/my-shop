116eae6c90c6f6c62726cccfecc0f2401794c379
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-08 21:30:53 +0100

    useCart store - refactoring e completamento

--- src/services/cart/useCart.ts
+++ src/services/cart/useCart.ts
@@ -15,7 +15,21 @@ export const useCart = create<CartState>((set, get) => ({
   list: [],
   addToCart: (product: Product) => {
     const found = get().list.find((item) => item.product.id === product.id);
-
+    if (found) {
+      get().increaseQty(product.id);
+    } else {
+      const item: CartItem = { product, qty: 1 };
+      // set({ list: [...get().list, item] });
+      set((state) => ({ list: [...state.list, item] }));
+    }
+  },
+  removeFromCart: (productId: string) => {
+    set((state) => ({
+      list: state.list.filter((item) => item.product.id !== productId),
+    }));
+  },
+  increaseQty: (productId: string) => {
+    const found = get().list.find((item) => item.product.id === productId);
     if (found) {
       found.qty++;
       set((state) => ({
@@ -23,14 +37,23 @@ export const useCart = create<CartState>((set, get) => ({
           return item.product.id === found?.product.id ? found : item;
         }),
       }));
-    } else {
-      const item: CartItem = { product, qty: 1 };
-      // set({ list: [...get().list, item] });
-      set((state) => ({ list: [...state.list, item] }));
     }
   },
-  removeFromCart: (productId: string) => {},
-  increaseQty: (productId: string) => {},
-  decreaseQty: (productId: string) => {},
-  clearCart: () => {},
+  decreaseQty: (productId: string) => {
+    const found = get().list.find((item) => item.product.id === productId);
+    if (found && found.qty === 1) {
+      get().removeFromCart(productId);
+    }
+    if (found && found.qty > 0) {
+      found.qty--;
+      set((state) => ({
+        list: state.list.map((item) => {
+          return item.product.id === found?.product.id ? found : item;
+        }),
+      }));
+    }
+  },
+  clearCart: () => {
+    set({ list: [] });
+  },
 }));
